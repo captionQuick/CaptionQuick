9429e2c96f78600a2aef8b076400d6a193f00a0c
maryam <maryam_moein@hotmail.com>
2024-07-16 23:42:42 +0330

    Initial Commit

---
+++ CaptionQuick/CaptionQuickApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptionQuickApp.swift
+//  CaptionQuick
+//
+//  Created by Maryam on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CaptionQuickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaptionQuick/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CaptionQuick
+//
+//  Created by Maryam on 7/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
