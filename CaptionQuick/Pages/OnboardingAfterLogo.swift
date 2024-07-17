3016a22863f3bc4ac09341029be15d59ab9b1e15
Peyman <peyman.osatian@gmail.com>
2024-07-17 22:51:40 +0200

    Logo Animation

---
+++ CaptionQuick/Pages/OnboardingAfterLogo.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingAfterLogo.swift
+//  CaptionQuick
+//
+//  Created by Peyman Osatian on 2024-07-17.
+//
+
+import SwiftUI
+
+struct OnboardingAfterLogo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingAfterLogo()
+}
---
+++ CaptionQuick/Pages/OnboardingLogo.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingLogo.swift
+//  CaptionQuick
+//
+//  Created by Peyman Osatian on 2024-07-17.
+//
+
+import SwiftUI
+
+struct OnboardingLogo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingLogo()
+}
