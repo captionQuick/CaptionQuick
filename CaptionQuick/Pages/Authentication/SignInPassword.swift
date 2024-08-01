0a02bbdcb8330389e7d3a066a3af127a580203bd
Peyman <peyman.osatian@gmail.com>
2024-08-01 19:50:09 +0200

    signinlogin

---
+++ CaptionQuick/Pages/Authentication/SignInPassword.swift
@@ -0,0 +1,18 @@
+//
+//  SignInPassword.swift
+//  CaptionQuick
+//
+//  Created by Peyman Osatian on 2024-08-01.
+//
+
+import SwiftUI
+
+struct SignInPassword: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInPassword()
+}
