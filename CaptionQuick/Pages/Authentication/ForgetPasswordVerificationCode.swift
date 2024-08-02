3ece88121696a54a5dd6964be4f30c6252c1fc94
Peyman <peyman.osatian@gmail.com>
2024-08-02 23:42:40 +0200

    forgetpassword

---
+++ CaptionQuick/Pages/Authentication/ForgetPasswordVerificationCode.swift
@@ -0,0 +1,18 @@
+//
+//  ForgetPasswordVerificationCode.swift
+//  CaptionQuick
+//
+//  Created by Peyman Osatian on 2024-08-02.
+//
+
+import SwiftUI
+
+struct ForgetPasswordVerificationCode: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgetPasswordVerificationCode()
+}
