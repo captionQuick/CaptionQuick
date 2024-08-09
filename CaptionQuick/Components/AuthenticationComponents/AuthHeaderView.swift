7670524b95db56031de03b134610d8b807d90d6d
Peyman <peyman.osatian@gmail.com>
2024-08-09 02:36:19 +0200

    form handling signup

---
+++ CaptionQuick/Components/AuthenticationComponents/AuthHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthHeaderView.swift
+//  CaptionQuick
+//
+//  Created by Peyman Osatian on 2024-08-09.
+//
+
+import SwiftUI
+
+struct AuthHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthHeaderView()
+}
---
+++ CaptionQuick/Components/AuthenticationComponents/SocialMediaIcons.swift
@@ -0,0 +1,18 @@
+//
+//  SocialMediaIcons.swift
+//  CaptionQuick
+//
+//  Created by Peyman Osatian on 2024-08-09.
+//
+
+import SwiftUI
+
+struct SocialMediaIcons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SocialMediaIcons()
+}
