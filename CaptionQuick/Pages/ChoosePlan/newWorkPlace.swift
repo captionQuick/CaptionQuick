c6e4fd7ec51630ea3244c7a4286a1f9a356995c8
Peyman <peyman.osatian@gmail.com>
2024-08-27 00:57:24 +0200

    workplace

---
+++ CaptionQuick/Pages/ChoosePlan/newWorkPlace.swift
@@ -0,0 +1,18 @@
+//
+//  newWorkPlace.swift
+//  CaptionQuick
+//
+//  Created by Peyman Osatian on 2024-08-27.
+//
+
+import SwiftUI
+
+struct newWorkPlace: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    newWorkPlace()
+}
